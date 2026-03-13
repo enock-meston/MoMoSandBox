7e6184b6c050d8e948a5022dea9bfad7f2a00b16
enock-meston <ndagijimanaenock11@gmail.com>
2026-03-13 14:33:44 +0200

    Add project files.

---
+++ MoMoSandBoxWeb/Components/Pages/payment/PaymentBase.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MoMoSandBoxWeb.Services;
+
+namespace MoMoSandBoxWeb.Components.Pages.payment
+{
+    public class PaymentBase : ComponentBase
+    {
+        public string phoneNumber = "56733123453";
+        public string amount = "600";
+        public string statusMessage = "";
+        public string responseDetails = "";
+        public bool isLoading = false;
+        public bool success = false;
+
+        [Inject]
+        public IJSRuntime? JSRuntime { get; set; }
+        [Inject]
+        public MoMoService MoMoService { get; set; }
+
+        // generate token
+        public async Task ProcessPayment()
+        {
+            //if (JSRuntime != null)
+            //{
+            //    await JSRuntime.InvokeVoidAsync("alert", "Payment function called!");
+            //}
+
+            isLoading = true;
+            statusMessage = "";
+            responseDetails = "";
+
+            try
+            {
+                var referenceId = Guid.NewGuid().ToString();
+                var (ok, response) = await MoMoService.RequestToPayAsync(
+                    amount: amount,
+                    currency: "EUR",
+                    externalId: "00004335",
+                    payerPhone: phoneNumber,
+                    referenceId: referenceId,
+                    payerMessage: "MoMo Market Payment",
+                    payeeNote: "MoMo Market Payment",
+                    bearerToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6IjcwZGMxZDBkLWJjNWItNGZjMy04MTBiLTlmYzk4YmI5MDg2NiIsImV4cGlyZXMiOiIyMDI2LTAzLTEzVDE0OjU3OjEzLjYyMjI2MDk5OCIsInNlc3Npb25JZCI6IjY5OGU4NDZmLWFkMDUtNGVjMy04YTdlLWM3MjMxNTg0OGMxYSJ9.IQBQrTQs9Z5TVBjusnjuJGm0-5m4C1Zd1w_oLEOfk7qMwAF2sEbWR4cSX1S3nE_J6elND2F3p2P5g0DN1fdepoLX6A-tkO7qIwx-wn6s5JAxnRv2j0SNBr2F9YO3F35XFtiiUKXa4HzPf7j4XCO5rVr0oLvNXJVqZPH7TJh9e3Vvdv_dpilM4cXjiv5brQE5xke0O8CzJYWBIof1-mkH6JmuMe4BfR-1Etq4qma-XjKtl9B7jUgGDAw1VB6xG0uLdzcR6vbN-C7YuKXxHIhAFOhPDFehJhm15yPimW1jlaCBWMiJp2kZ6Ws2wYVQ8IqUEyusUuRCAmkBGHNRj7ri6Q",
+                    subscriptionKey: "25407f5a9eae40528115c71fe5205961"
+                );
+
+                success = ok;
+                responseDetails = response;
+
+                if (ok)
+                {
+                    //statusMessage = $"Payment request sent successfully! (202 Accepted)";
+                    //call  status Request To Pay method
+                    await ProcessCheckStatus(referenceId);
+                }
+                else
+                {
+                    statusMessage = $"Payment failed!{response.Count()}";
+                }
+            }
+            catch (Exception ex)
+            {
+                success = false;
+                statusMessage = $"❌ Error: {ex.Message}";
+                responseDetails = ex.ToString();
+            }
+            finally
+            {
+                isLoading = false;
+            }
+        }
+
+        public async Task ProcessCheckStatus(string referenceId)
+        {
+            isLoading = true;
+            statusMessage = "";
+            responseDetails = "";
+            try
+            {
+                var (ok, responseBody) = await MoMoService.GetRequestToPayStatusAsync(
+                    referenceId: "158c8952-23e9-490b-b12d-75953200bf12",
+                    bearerToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6IjcwZGMxZDBkLWJjNWItNGZjMy04MTBiLTlmYzk4YmI5MDg2NiIsImV4cGlyZXMiOiIyMDI2LTAzLTEzVDE0OjU3OjEzLjYyMjI2MDk5OCIsInNlc3Npb25JZCI6IjY5OGU4NDZmLWFkMDUtNGVjMy04YTdlLWM3MjMxNTg0OGMxYSJ9.IQBQrTQs9Z5TVBjusnjuJGm0-5m4C1Zd1w_oLEOfk7qMwAF2sEbWR4cSX1S3nE_J6elND2F3p2P5g0DN1fdepoLX6A-tkO7qIwx-wn6s5JAxnRv2j0SNBr2F9YO3F35XFtiiUKXa4HzPf7j4XCO5rVr0oLvNXJVqZPH7TJh9e3Vvdv_dpilM4cXjiv5brQE5xke0O8CzJYWBIof1-mkH6JmuMe4BfR-1Etq4qma-XjKtl9B7jUgGDAw1VB6xG0uLdzcR6vbN-C7YuKXxHIhAFOhPDFehJhm15yPimW1jlaCBWMiJp2kZ6Ws2wYVQ8IqUEyusUuRCAmkBGHNRj7ri6Q",
+                    subscriptionKey: "25407f5a9eae40528115c71fe5205961"
+                );
+
+                success = ok;
+                responseDetails = responseBody;
+
+                if (ok)
+                {
+                    statusMessage = "Status: " + responseBody;
+                }
+                else
+                {
+                    statusMessage = "Error: " + responseBody;
+                }
+            }
+            catch (Exception ex)
+            {
+                success = false;
+                statusMessage = $"❌ Error: {ex.Message}";
+                responseDetails = ex.ToString();
+            }
+            finally
+            {
+                isLoading = false;
+            }
+        }
+        protected override async Task OnInitializedAsync()
+        {
+
+        }
+    }
+}
---
+++ MoMoSandBoxWeb/Services/MoMoService.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace MoMoSandBoxWeb.Services
+{
+    public class MoMoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public MoMoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<(bool Success, string Response)> RequestToPayAsync(
+            string amount,
+            string currency,
+            string externalId,
+            string payerPhone,
+            string referenceId,
+            string payerMessage,
+            string payeeNote,
+            string bearerToken,
+            string subscriptionKey){
+            //partyId  is payerPhone
+            //var referenceId = Guid.NewGuid().ToString();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://sandbox.momodeveloper.mtn.com//collection/v1_0/requesttopay");
+            request.Headers.Add("X-Reference-Id", referenceId);
+            request.Headers.Add("X-Target-Environment", "sandbox");
+            request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            request.Headers.Add("Authorization", "Bearer " + bearerToken);
+
+            var content = new StringContent("{\r\n  \"amount\": \"" + amount + "\",\r\n  \"currency\": \"" + currency + "\",\r\n  \"externalId\": \"" + externalId + "\",\r\n  \"payer\": {\r\n    \"partyIdType\": \"MSISDN\",\r\n    \"partyId\": \"" + payerPhone + "\"\r\n  },\r\n  \"payerMessage\": \"" + payerMessage + "\",\r\n  \"payeeNote\": \"" + payeeNote + "\"\r\n}", null, "application/json");
+            request.Content = content;
+
+            var response = await _httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {   
+                
+                return (true, await response.Content.ReadAsStringAsync());
+                
+            }
+            else
+            {
+                return (false, await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        //check Status request to pay method here
+        public async Task<(bool Success, string Response)> GetRequestToPayStatusAsync(
+            string referenceId,
+            string bearerToken,
+            string subscriptionKey)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                $"https://sandbox.momodeveloper.mtn.com/collection/v1_0/requesttopay/{referenceId}");
+
+            request.Headers.Add("X-Target-Environment", "sandbox");
+            request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            request.Headers.Add("Authorization", "Bearer " + bearerToken);
+
+            var response = await _httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return (true, await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                return (false, await response.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
